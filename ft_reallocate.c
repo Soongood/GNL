ec40c5eda63619a89d6ae2d6d1a5e5e666d6be53
Soongood <46536006+Soongood@users.noreply.github.com>
2019-10-07 08:52:03 +0300

    Update ft_reallocate.c

--- ft_reallocate.c
+++ ft_reallocate.c
@@ -1,6 +1,12 @@
 #include "libft.h"
 
-void	*ft_reallocate(void *ptr, size_t size)
+static	void	ft_freeptr(void *pointer)
+{
+	free(pointer);
+	pointer = NULL;
+}
+
+void		*ft_reallocate(void *ptr, size_t size)
 {
 	void		*p;
 	void		*temp;
@@ -9,15 +15,13 @@ void	*ft_reallocate(void *ptr, size_t size)
 	if (!ptr)
 		return (ptr = ft_memalloc(size));
 	if (!size && ptr)
-			free(ptr);
-	else
+		return(ft_freeptr(ptr));
+	if (p = ft_memalloc(size))
 	{
-		if (p = ft_memalloc(size))
-		{
-			length = (ft_strlen((const char *)ptr) >= size) ? size : ft_strlen((const char *)ptr);
-			temp = ft_memcpy(p, ptr, length);
-			return (temp);
-		}
+		length = (ft_strlen((const char *)ptr) >= size) ? size : ft_strlen((const char *)ptr);
+		temp = ft_memcpy(p, ptr, length);
+		ft_freeptr(ptr);
+		return (temp);
 	}
 	return (NULL);
 }
