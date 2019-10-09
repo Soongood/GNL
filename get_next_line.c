a285a894ff1db0b5a2d0339bf6a366077b01ff66
Soongood <46536006+Soongood@users.noreply.github.com>
2019-10-09 22:32:33 +0300

    Add files via upload

--- get_next_line.c
+++ get_next_line.c
@@ -6,16 +6,33 @@
 /*   By: trobbin <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/25 17:41:57 by trobbin           #+#    #+#             */
-/*   Updated: 2019/09/25 18:08:03 by trobbin          ###   ########.fr       */
+/*   Updated: 2019/10/09 22:31:15 by trobbin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdio.h>
+#include "get_next_line.h"
 
-int main()
+char *ft_readline(char *buffer)
 {
-	ft_putchar('\n');
-	printf("Hello");
-	return (0);
+	while (*buffer)
+	{
+
+	}
+}
+
+int get_next_line(const int fd, char **line)
+{
+	static char	*buf[FD_SETSIZE];
+	int		count;
+	int		bytes;
+
+	if (fd >= FD_SETSIZE || fd < 0 || !line)
+		return (-1);
+	if (buf[fd])
+		//read
+	while ((bytes = read(fd, buf, BUFF_SIZE)))
+	{
+		ft_realloc();
+	}
 }
