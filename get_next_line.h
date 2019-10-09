5bd64e2fafaa89d2835d6fb7ab16b6bf6a70c54f
Soongood <46536006+Soongood@users.noreply.github.com>
2019-10-09 22:32:43 +0300

    Add files via upload

---
+++ get_next_line.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: trobbin <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/09/29 07:30:51 by trobbin           #+#    #+#             */
+/*   Updated: 2019/10/09 22:21:45 by trobbin          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef _GET_NEXT_LINE_H
+# define _GET_NEXT_LINE_H
+#  define BUFF_SIZE 512
+
+# include <sys/types.h>
+
+int	get_next_line(const int fd, char **line);
+
+#endif
