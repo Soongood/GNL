3d7feca6f528387d9219efec549ca59ee5f5a728
Soongood <46536006+Soongood@users.noreply.github.com>
2019-10-10 09:08:32 +0300

    Update get_next_line.c

--- get_next_line.c
+++ get_next_line.c
@@ -13,26 +13,34 @@
 #include "libft.h"
 #include "get_next_line.h"
 
-char *ft_readline(char *buffer)
+int	ft_readline(char *buffer)
 {
-	while (*buffer)
-	{
-
-	}
+	int shift;
+	
+	shift = 0;
+	while (buffer[shift] != '\n')
+		if (!buffer[shift++])
+			return (0);
+	ft_reallocate((buffer + shift), ft_strlen(buffer) - shift);
+	return (1);
 }
 
-int get_next_line(const int fd, char **line)
+int	get_next_line(const int fd, char **line)
 {
-	static char	*buf[FD_SETSIZE];
-	int		count;
+	static char	*buf[FD_SETSIZE + 1];
+	int		temp;
 	int		bytes;
 
 	if (fd >= FD_SETSIZE || fd < 0 || !line)
 		return (-1);
 	if (buf[fd])
-		//read
+		//read and write to line (function)
+	temp = 0;
 	while ((bytes = read(fd, buf, BUFF_SIZE)))
 	{
-		ft_realloc();
+		temp += bytes;
+		buf[fd] = ft_reallocate(buf, temp);
 	}
+	//read and write to line (function)
+	return (0);
 }
