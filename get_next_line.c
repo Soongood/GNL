f1165b10142106727df17d423a3b5de83a6ddd9a
Soongood <46536006+Soongood@users.noreply.github.com>
2019-10-25 01:28:04 +0300

    Add files via upload

--- get_next_line.c
+++ get_next_line.c
@@ -20,42 +20,47 @@ int	ft_readline(char **buffer, char **line, int file_d)
 	char	temp_buffer[BUFF_SIZE];
 
 	shift = 0;
-	while ((*buffer)[shift] != '\n')
+	while (!ft_strlen(*buffer) || (*buffer)[shift] != '\n')
 	{
 		if (!(*buffer)[shift++])
 		{
 			if ((bytes = read(file_d, temp_buffer, BUFF_SIZE)))
 			{
 				*buffer = ft_realloc(*buffer, ft_strlen(*buffer) + BUFF_SIZE + 1);
-				temp_ptr = ft_strcpy((*buffer) + ft_strlen(*buffer), temp_buffer);
+				temp_ptr = ft_memcpy((*buffer) + ft_strlen(*buffer), temp_buffer, bytes);
 				return (ft_readline(buffer, line, file_d));
 			}
 			*line = ft_strdup(*buffer);
-			free(*buffer);
+			ft_memdel((void **)buffer);
 			return (1);
 		}
 	}
-	*line = ft_strsub(*buffer, 0, shift);
+	*line = ft_strsub(*buffer, 0, (!shift) ? 1 : shift);
 	temp_ptr = ft_strsub((*buffer) + shift + 1, 0, ft_strlen(*buffer) - shift);
-	free(*buffer);
+	ft_memdel((void **)buffer);
 	*buffer = temp_ptr;
 	return (1);
 }
 
 int	get_next_line(const int fd, char **line)
 {
-	static char	*buf[_SC_OPEN_MAX + 1];
+	static char	*buf[FD_SIZE + 1];
 	int		bytes;
 
-	if (fd >= _SC_OPEN_MAX || fd < 0 || !line || !BUFF_SIZE)
+	if (fd >= FD_SIZE || fd < 0 || !line || !BUFF_SIZE)
 		return (-1);
 	if (!buf[fd])
 	{
 		buf[fd] = ft_memalloc(BUFF_SIZE + 1);
 		if ((bytes = read(fd, buf[fd], BUFF_SIZE)) <= 0)
-			return (-1);
+		{
+			ft_memdel((void **)&buf[fd]);
+			if (bytes < 0)
+				return (-1);
+			*line = NULL;
+			return (0);	
+		}
 	}
-	if (*buf[fd])
+	if (buf[fd])
 		return (ft_readline(&buf[fd], line, fd));
-	return (0);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -13,9 +13,11 @@
 #ifndef _GET_NEXT_LINE_H
 # define _GET_NEXT_LINE_H
 #  define BUFF_SIZE 512
+#   define FD_SIZE 1024
 
 # include "libft/libft.h"
 
+int ft_readline(char **buffer, char **line, int fd);
 int	get_next_line(const int fd, char **line);
 
 #endif
